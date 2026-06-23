11c6a1531a19b599b7f669c3997cbd4d725e472e
lujingxiang <lujingxiang@users.noreply.github.com>
2026-06-23 14:38:00 +0800

    feat: add host 0.0.0.0 binding for LAN access, update pages and agents

--- get-think-chain.py
+++ get-think-chain.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 client = Client(api_key=os.environ["LANGCHAIN_API_KEY"])
 # 填入你截图中的 Run ID (通常在 URL 中可以看到)
-run_id = "019ed3a0-c425-7da2-b6f8-9a912fc5e3ea" 
+run_id = "019eed74-81b7-7080-9f05-0954e0b7c92d" 
 
 def get_full_trace(run_id):
     # 获取当前节点
